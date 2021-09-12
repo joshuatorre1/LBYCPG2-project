35b62e5b7c5d0667dbcbffd516fe1df03bf9f7e7
joshuatorre1 <63407715+joshuatorre1@users.noreply.github.com>
2021-09-12 16:02:47 +0800

    Updated manage.php
    Added registration box

--- Project/manage.php
+++ Project/manage.php
@@ -32,9 +32,40 @@
                     <input type="submit" class="button" value="Log In">
                 </div>
                 <div class="link-container">
-                    <a href="#">Don't have an account yet? Sign up here.</a>
+                    <a href="#register">Don't have an account yet? Sign up here.</a>
                 </div>
 
             </form>
         </div>
+
+        <div class="container" id="register">
+            <form id="reg-form" class="forms" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="h1-container"><h1>REGISTER YOUR ORGANIZATION</h1></div>
+                <fieldset>
+                    <legend>BASIC INFORMATION</legend>
+                    <div class="text-field">
+                        <input class="field" type="text" placeholder=" " id="username" name="username" required>
+                        <label class="prompt" for="username">Organization Name</label>
+                    </div>
+                    <div class="text-field">
+                        <input class="field" type="password" placeholder=" " id="password" name="password" required>
+                        <label class="prompt" for="password">Organization Address</label>
+                    </div>
+                </fieldset>
+                <!--                <div class="text-field">-->
+                <!--                    <input class="field" type="text" placeholder=" " id="username" name="username" required>-->
+                <!--                    <label class="prompt" for="username">Username</label>-->
+                <!--                </div>-->
+                <!--                <div class="text-field">-->
+                <!--                    <input class="field" type="password" placeholder=" " id="password" name="password" required>-->
+                <!--                    <label class="prompt" for="password">Password</label>-->
+                <!--                </div>-->
+                <!--                <div class="button-container">-->
+                <!--                    <input type="submit" class="button" value="Log In">-->
+                <!--                </div>-->
+                <!--                <div class="link-container">-->
+                <!--                    <a href="register.php">Don't have an account yet? Sign up here.</a>-->
+                <!--                </div>-->
+
+            </form>
     </body>
